32be2b69a0439811b5f485024afcd1cec2470619
AlexisRoe <A.Roehrling@gmx.de>
2020-10-30 10:39:01 +0100

    Create set to localStorage

---
+++ src/utils/storage.js
@@ -0,0 +1,19 @@
+export default function addToDo (todo) {
+
+    let existingToDo = null;
+
+    try {
+        existingToDo = JSON.parse(localStorage.getItem("todo")) || [];
+    } catch (error){
+        console.error(error);
+        existingToDo = [];
+    } 
+
+    if (!existingToDo.includes(todo)) {
+        const newToDo =  [...existingToDo, todo];
+        localStorage.setItem("todo", JSON.stringify(newToDo));
+        return newToDo;
+    }
+
+    return existingToDo;
+}
