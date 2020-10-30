a1e08c81374f57d40497b27d9a6f1366bb0df8fb
mariegross <marie.gross@gmx.de>
2020-10-30 14:53:12 +0100

    Add checkbox and storage function

--- src/components/OutputField.js
+++ src/components/OutputField.js
@@ -1,6 +1,18 @@
 import React from "react";
 import "./OutputField.css";
+import checkedToDo from "../utils/checked";
 
 export default function OutputField({ todo }) {
-  return <div>{todo}</div>;
+  return (
+    <div>
+      <input
+        type="checkbox"
+        name="nameToDo"
+        onClick={() => {
+          checkedToDo(todo);
+        }}
+      />
+      <label htmlFor="nameToDo">{todo}</label>
+    </div>
+  );
 }
