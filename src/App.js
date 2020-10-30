5933b1e349c5a6c9c3a3fe45d27f6fd142d2f241
AlexisRoe <A.Roehrling@gmx.de>
2020-10-30 09:43:26 +0100

    Initial cleanup

--- src/App.js
+++ src/App.js
@@ -1,31 +1,7 @@
-import React, { useEffect } from "react";
-import "./app.css";
-import Game from "./components/Game";
+import './app.css';
 
 function App() {
-  useEffect(() => {
-    window.addEventListener("mousedown", function () {
-      document.body.classList.add("mouse-navigation");
-      document.body.classList.remove("kbd-navigation");
-    });
-    window.addEventListener("keydown", function (e) {
-      if (e.key === "Tab") {
-        document.body.classList.add("kbd-navigation");
-        document.body.classList.remove("mouse-navigation");
-      }
-    });
-    window.addEventListener("click", function (e) {
-      if (e.target.tagName === "A" && e.target.getAttribute("href") === "#") {
-        e.preventDefault();
-      }
-    });
-  }, []);
-
-  return (
-    <div className="app">
-      <Game />
-    </div>
-  );
-}
+    return (<>< />)
+};
 
 export default App;
--- src/components/Square.js
+++
@@ -1,6 +0,0 @@
-import "./square.css";
-import React from "react";
-
-export default function Square() {
-  return <button className="square">{/* TODO */}</button>;
-}
