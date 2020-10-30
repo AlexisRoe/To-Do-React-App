4dff0bdc6497757336d5528fafa9d4c1db8d127e
AlexisRoe <A.Roehrling@gmx.de>
2020-10-30 14:14:57 +0100

    Create list view

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
-import "./app.css";
-import React from "react";
-import InputField from "./components/InputField";
-import OutputFieldList from "./components/OutputFieldList";
+import './app.css';
+import React, { useState } from 'react';
+import InputField from './components/InputField';
+import OutputFieldList from './components/OutputFieldList';
+import addToDo from './utils/storage';
 
 function App() {
-  const toDoList = JSON.parse(localStorage.getItem("todo")) || [];
-  return (
-    <main>
-      <InputField />
-      <OutputFieldList toDoList={toDoList} />
-    </main>
-  );
+    const defaultList = JSON.parse(localStorage.getItem("todo")) || ["Nix zu tun"];
+
+    const [toDoList, setToDo] = useState(defaultList);
+
+    return (
+        <main>
+            <InputField
+                onSubmit={(event) => {
+                    event.preventDefault();
+                    // hier später ersetzen durch bessere Form!
+                    let inputValue = document.querySelector('#inputField').value;
+                    const newList = addToDo(inputValue);
+                    inputValue = '';
+                    setToDo(newList);
+                }}
+            />
+            {toDoList && 
+            <OutputFieldList toDoList={toDoList} />}
+        </main>
+    );
 }
 
 export default App;
