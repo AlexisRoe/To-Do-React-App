e7f1910d566dddfb512d7952ee23a0be76b841a5
AlexisRoe <A.Roehrling@gmx.de>
2020-11-01 01:05:06 +0100

    Add completed list

--- src/App.js
+++ src/App.js
@@ -24,16 +24,23 @@ function App() {
             setItemList([]);
         } else if (/^list todo/.test(inputValue)) {
             setItemList(getData('todo'));
+        } else if (/^list complete/.test(inputValue)) {
+            const completedList = getData("deleted")
+            setBuildSwitch("complete");
+            setItemList(completedList);
         } else if (/^help/.test(inputValue)) {
             const help = [
-                '$ add text           = add new to-do item with value=text',
-                '$ list todo          = shows to-do list',
+                '$ text              = add new to-do item with value=text',
+                '$ list todo         = shows to-do list',
+                '$ list complete     = shows all completed to-do´s',
+                '$ filter checked    = shows only checked to-do',
+                '$ filter unchecked  = shows only unchecked to-do',
                 '$ check number      = check item with #number',
                 '$ uncheck number    = uncheck item with #number',
                 '$ complete number   = delete item with #number',
-                '$ clear              = clear console',
+                '$ clear             = clear console',
             ];
-            setBuildSwitch("help")
+            setBuildSwitch("help");
             setItemList(help);
         } else if (/^complete/.test(inputValue)) {
             const index = inputValue.match(/\d+/) -1;
@@ -60,8 +67,8 @@ function App() {
                 <header className='header'>
                     ******************************************************
                     <br />
-                    *** JUST ANOTHER STUPID TO-DO-APP *** <br />
-                    type help for more information
+                    JUST ANOTHER STUPID TO-DO-APP<br />
+                    2020-10-31
                     <br />
                     ******************************************************
                 </header>
--- src/components/OutputFieldList.js
+++ src/components/OutputFieldList.js
@@ -2,12 +2,16 @@ import React from 'react';
 import './OutputFieldList.css';
 import OutputField from './OutputField';
 import Help from './Help';
+import Completed from "./Completed";
 
 export default function OutputFieldList({ itemList, onRemove, router }) {
     function output() {
         if (router === 'help') {
             const helpList = itemList.map((item) => <Help key={item} helpValue={item} />);
             return helpList;
+        } else if (router === "complete") {
+            const completedList = itemList.map((item) => <Completed key={item} completedToDo={item}/>);
+            return completedList;
         } else {
             const regularList = itemList.map((item, index) => (
                 <OutputField key={item} todo={item} onRemove={onRemove} index={index} />
